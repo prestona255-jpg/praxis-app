969d851fb0eafeadf0e1844a552f6b198c52ecc1
Preston Allen <prestona255@gmail.com>
2026-05-22 09:58:01 -0500

    Revert "Stage 5.4 Stage 2b-1: CACHE_VERSION bump to praxis-v3.11-c"
    This reverts commit 24b75c4c2bcd8df47fc86af91c3604f17a0d6ef0.

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // let, arrow, class, or template literals anywhere.
 // =====================================================================
 
-var CACHE_VERSION = 'praxis-v3.11-c';
+var CACHE_VERSION = 'praxis-v3.11-b';
 
 var APP_SHELL = [
   '/',
