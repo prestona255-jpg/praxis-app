1a4804550a7c36e9c811804ad074c6da3fbb150e
Preston Allen <prestona255@gmail.com>
2026-06-25 17:04:25 -0500

    Build 1: bump CACHE_VERSION to v3.149 for ship

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // let, arrow, class, or template literals anywhere.
 // =====================================================================
 
-var CACHE_VERSION = 'praxis-v3.148';
+var CACHE_VERSION = 'praxis-v3.149';
 
 var APP_SHELL = [
   '/',
